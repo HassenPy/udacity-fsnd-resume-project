496aa303f82d0b30e5f5d76b6894b497fa7918b4
Hassen ben tanfous <hbt.youtube@Yahoo.com>
2017-09-08 00:31:46 +0100

    Add JS comments \__(-0-)_/

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,12 @@ var activeSection;
 var activeNav;
 
 function toggleSection(e) {
+  // Add an active class to the clicked nav item and display its relative content section
   var target = document.querySelector(this.dataset.target);
   if (target.style.display !== "block") target.style.display = "block";
   if (!this.classList.contains("active")) this.classList.add("active");
 
+  // Hide the already active content section.
   if (this !== activeNav) {
     activeSection.style.display = "none";
     activeNav.classList.remove("active");
@@ -15,13 +17,16 @@ function toggleSection(e) {
 }
 
 function bindButtons() {
+  // Initialize the global states used to track active section and nav link.
   activeSection= document.querySelector("#projects");
   activeNav = document.querySelector(".nav-link");
+
   document.querySelectorAll(".nav-link").forEach(function(el){
     el.addEventListener("click", toggleSection);
   });
 }
 
+// http://youmightnotneedjquery.com/#ready
 if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
   bindButtons();
 } else {
